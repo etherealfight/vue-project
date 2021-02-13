98a7fc7cf4f3aebe3dbcb7fe175d2a121a508e70
etherealfight <60640545+etherealfight@users.noreply.github.com>
2021-02-13 23:40:40 +0800

    publish

--- src/components/profile/publishLearn.vue
+++ src/components/profile/publishLearn.vue
@@ -2,18 +2,35 @@
 * @FileDescription:发布组件，用于用户发表新文章
 -->
 <template>
-  <div class="publishRewardBox">
-    <div class="publishRewardMain">
+  <div class="publishLearnBox">
+    <div class="publishLearnMain">
       <i class="el-icon-back" @click="back"></i>
+      <span>发布学习资源</span>
       <textarea
         name="发布"
-        id="publishReward"
-        v-model="publishRewardText"
+        id="publishLearn"
+        v-model="publishLearnText"
         cols="50"
-        rows="15"
+        rows="10"
         placeholder="请输入发布内容"
       ></textarea>
-      <el-button round id="publishRewardButton" @click="publishReward"
+      <div class="upload">
+        <el-upload
+          class="uploaditem"
+          action="https://jsonplaceholder.typicode.com/posts/"
+          :auto-upload="false"
+          :limit="9"
+          list-type="picture-card"
+          :on-preview="handlePictureCardPreview"
+          :on-remove="handleRemove"
+        >
+          <i class="el-icon-plus"></i>
+        </el-upload>
+        <el-dialog :visible.sync="dialogVisible">
+          <img width="100%" :src="dialogImageUrl" alt="" />
+        </el-dialog>
+      </div>
+      <el-button round id="publishLearnButton" @click="publishLearn" 
         >发布</el-button
       >
     </div>
@@ -24,7 +41,9 @@
 export default {
   data() {
     return {
-      publishRewardText: "",
+      publishLearnText: "",
+      dialogImageUrl: "",
+      dialogVisible: false,
     };
   },
   props: {
@@ -37,24 +56,27 @@ export default {
     /**
      * 发布文章
      */
-    async publishReward() {
+    async publishLearn() {
       let that = this;
       if (this.$store.state.userName == "") this.$message.warning("请先登录");
-      else if (that.publishRewardText == "")
+      else if (that.publishLearnText == "")
         this.$message.warning("发布内容不可为空");
       else {
-        console.log(this.publishRewardText);
+        console.log(this.publishLearnText);
         this.$emit("child-ok");
-        console.log(this.propsItem.isMask);
-        this.propsItem.isMask = false;
-        console.log(this.propsItem.isMask);
         // this.$message.info(res.msg);
         // console.log(res)
       }
     },
     back() {
       this.$emit("child-back");
-      this.propsItem.isMask = false;
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
     },
   },
   components: {},
@@ -64,49 +86,67 @@ export default {
 <style scoped>
 .el-icon-back {
   font-size: 3rem;
-  margin-left: -32rem;
-  margin-bottom: 2rem;
+  margin-left: -28rem;
 }
-.publishRewardBox {
+.publishLearnBox {
   height: 100vh;
   width: 100vw;
   background: rgb(240, 240, 240);
-  opacity: 1;
   z-index: 20;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 }
-.publishRewardMain {
+.publishLearnMain {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
-  padding: 50rem;
+  padding: 3rem;
   z-index: 20;
   background: white;
   opacity: 1;
   box-sizing: border-box;
-  overflow: scroll;
 }
-#publishReward {
+.publishLearnMain span {
+  font-size: 2rem;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  margin-bottom: 1rem;
+}
+#publishLearn {
   width: 80vw;
   padding: 1rem;
   font-family: "宋体";
   outline: none;
   box-sizing: border-box;
 }
-#publishRewardButton {
-  margin-top: 1rem;
-  background: #409eff !important;
-  color: white !important;
-  padding: 0 !important;
-  width: 8rem;
-  height: 3.5rem;
-  outline: none;
-  font-size: 1rem;
+.uploaditem >>> .el-upload-list {
+  width: 100vw;
+  padding: 2rem 0 0 3rem;
   box-sizing: border-box;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 11rem);
+}
+.uploaditem >>> .el-upload--picture-card {
+  margin-top: 1rem;
+  margin-left: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10rem !important;
+  height: 10rem !important;
+  line-height: 0 !important;
+}
+.uploaditem >>> .el-upload-list--picture-card .el-upload-list__item {
+  width: 10rem !important;
+  height: 10rem !important;
+  line-height: 0 !important;
+  margin: 0.25rem 0.5rem 0.5rem 0;
+}
+#publishLearnButton {
+  margin-top: 1rem;
+  width: 8rem;
 }
 </style>
