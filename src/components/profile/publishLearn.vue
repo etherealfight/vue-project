2e6b89209a39229a5ca89c86380ff6f81eaf010c
etherealfight <60640545+etherealfight@users.noreply.github.com>
2021-02-22 23:43:23 +0800

    chat

--- src/components/profile/publishLearn.vue
+++ src/components/profile/publishLearn.vue
@@ -44,9 +44,6 @@
           <el-button slot="trigger" size="small" type="primary"
             >选取文件</el-button
           >
-          <div slot="tip" class="el-upload__tip">
-            只能上传jpg/png文件，且不超过500kb
-          </div>
         </el-upload>
       </div>
       <el-button round id="publishLearnButton" @click="publishLearn"
--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ Vue.use(VueRouter)
 
 const routes = [{
     path: '/',
-    redirect: '/login'
+    redirect: '/main'
   },
   {
     path: '/main',
@@ -122,7 +122,7 @@ const routes = [{
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes
 })
