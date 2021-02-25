acabf121f805ac818579fd7dc893e2c305d3319f
etherealfight <60640545+etherealfight@users.noreply.github.com>
2021-02-25 22:55:47 +0800

    chat

--- src/components/chat/chatDetail.vue
+++ src/components/chat/chatDetail.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="chatDetailBox">
-    <div class="header"><i class="el-icon-back" @click="back"></i><span>{{objectname}}</span></div>
+    <div class="header">
+      <i class="el-icon-back" @click="back"></i><span>{{ objectname }}</span>
+    </div>
     <div class="main"></div>
-    <div class="footer"></div>
+    <div class="footer">
+      <el-input v-model="input"></el-input
+      ><el-button type="info" round class="inputButton">发送</el-button>
+    </div>
   </div>
 </template>
 
@@ -19,6 +24,7 @@ export default {
         { person: 0, context: "hhhhhhhh" },
         { person: 1, context: "66666666" },
       ],
+      input: "",
     };
   },
   methods: {
@@ -35,19 +41,41 @@ export default {
   height: 100vh;
   background: rgb(240, 240, 240);
 }
-.chatDetailBox .header{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 5rem;
+.chatDetailBox .header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
 }
-.chatDetailBox .header span{
-    font-size: 1.5rem;
+.chatDetailBox .header span {
+  font-size: 1.5rem;
 }
 .el-icon-back {
   position: absolute;
   font-size: 3rem;
   left: 5%;
   top: 2%;
+}
+.footer {
+  width: 100vw;
+  padding: 1rem;
+  box-sizing: border-box;
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: space-around;
+}
+.footer .el-input >>> .el-input__inner {
+  width: 70vw;
+  height: 4rem;
+  border-radius: 3rem;
+}
+.footer >>> .el-button {
+  width: 20vw;
+  height: 4rem;
+  border-radius: 3rem;
+}
+.footer >>> .el-button span{
+  font-size: 1rem;
 }
 </style>
